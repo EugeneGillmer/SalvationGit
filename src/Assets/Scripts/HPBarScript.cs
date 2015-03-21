9a22e96cc5b21674975d7537f5e920c9519830de
EugeneGillmer <911mockingbird@gmail.com>
2015-03-21 04:11:28 +0200

    MaxHP, HPBar Update
    Added maxHP stat and some UI and in-progress script for payer HP bar

--- src/Assets/Scripts/HPBarScript.cs
+++ src/Assets/Scripts/HPBarScript.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 public class HPBarScript : MonoBehaviour {
 
 
-    public Image HPBar;
+    public Image HPBarRect;
+    public Image HPBarCircle;
     public GameObject character;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +20,22 @@ public class HPBarScript : MonoBehaviour {
     {
         if (character != null)
         {
-            HPBar.rectTransform.sizeDelta = new Vector2((float)character.GetComponent<Stats>().HP, HPBar.GetComponent<RectTransform>().sizeDelta.y);
-            if (character.GetComponent<Stats>().HP < 0 && character.tag!="Player")
+            if (character.tag == "Player")
+            {
+                if (character.GetComponent<Stats>().HP > 0.5 * character.GetComponent<Stats>().MaxHP)
+                {
+                    HPBarRect.rectTransform.sizeDelta = new Vector2((float)character.GetComponent<Stats>().HP / character.GetComponent<Stats>().MaxHP * 100, HPBarRect.GetComponent<RectTransform>().sizeDelta.y);
+                }
+                else
+                {
+                    HPBarCircle.fillAmount = (float)character.GetComponent<Stats>().HP / character.GetComponent<Stats>().MaxHP * 100;
+                }
+            }
+            else
+            {
+                HPBarRect.rectTransform.sizeDelta = new Vector2((float)character.GetComponent<Stats>().HP, HPBarRect.GetComponent<RectTransform>().sizeDelta.y);
+            }
+            if (character.GetComponent<Stats>().HP <= 0 && character.tag != "Player")
             {
                 Destroy(character.gameObject);
             }
--- src/Assets/Scripts/Stats.cs
+++ src/Assets/Scripts/Stats.cs
@@ -5,6 +5,7 @@ public class Stats : MonoBehaviour
 {
 
     public int HP = 100;
+    public int MaxHP = 100;
     public int ATK = 50;
     public int coins = 50;
     //LATER add player stats 
