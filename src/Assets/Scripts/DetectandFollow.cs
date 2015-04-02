1bc0d1ee751073f806dd9dc597bf2fc7dd6b41c5
EugeneGillmer <911mockingbird@gmail.com>
2015-04-02 23:55:20 +0200

    Detect&Follow, Monkeytroop
    Wrote a new script, made a troop of monkeys with one boss monkey which
    follow and hit you, beware!

---
+++ src/Assets/Scripts/DetectandFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectandFollow : MonoBehaviour {
+
+
+    public int moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void OnTriggerStay(Collider thing)
+    {
+        if (thing.tag == "Player")
+            transform.position = Vector3.MoveTowards(transform.position, thing.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
