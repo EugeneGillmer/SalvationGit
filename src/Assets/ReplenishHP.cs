8d2d4a806738e62b2f4552bd43801e28a3b191c0
EugeneGillmer <911mockingbird@gmail.com>
2015-04-02 10:51:34 +0200

    Beautification, ReplenishHP.
    Added trees & more grass, added new particles for OurTree and added a
    script to replenish HP

---
+++ src/Assets/ReplenishHP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplenishHP : MonoBehaviour {
+
+    public float replenishAmount = 10;
+
+    void OnTriggerStay(Collider thing)
+    {
+        if (thing.gameObject.GetComponent<Stats>() != null)
+        {
+            thing.gameObject.GetComponent<Stats>().HP += replenishAmount * Time.deltaTime;
+
+            if (thing.gameObject.GetComponent<Stats>().HP >= thing.gameObject.GetComponent<Stats>().MaxHP)
+                thing.gameObject.GetComponent<Stats>().HP = thing.gameObject.GetComponent<Stats>().MaxHP;
+        }
+    }
+}
